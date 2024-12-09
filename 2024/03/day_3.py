de12f5e210957feb8255a7fb900126fcab626d12
matteo-ticli <teoticli@me.com>
2024-12-09 01:14:54 +0100

    solution day3, small refactor

---
+++ 2024/03/day_3.py
@@ -0,0 +1,89 @@
+from day_3_input import str_03
+
+
+def summation_mul(l: list) -> int:
+    tot = 0
+    for i in l:
+        tot += i[0] * i[1]
+    return tot
+
+
+def compose_number(match: str, i: int):
+    num = ""
+    i += 1
+    while True:
+        try:
+            int(match[i])
+            num += match[i]
+            i += 1
+            if match[i] == ",":
+                X = int(num)
+                num = ""
+                i += 1
+                while True:
+                    try:
+                        int(match[i])
+                        num += match[i]
+                        i += 1
+                        if match[i] == ")":
+                            Y = int(num)
+                            return X, Y
+                    except:
+                        return 0, 0
+        except:
+            return 0, 0
+
+
+def find_match(m: str, i: int, match: str = "mul(X,Y)"):
+    j = 0
+    while i < len(m):
+        if m[i] == match[j]:
+            if match[j+1] == "(":
+                i += 1 # skip (
+                return compose_number(m, i)
+            i += 1
+            j += 1
+        else:
+            return 0, 0
+
+
+def mul_flag(m: str, i: int, do: str = "do()", dn: str = "don't()"):
+    len_m, len_do, len_dn = len(m), len(do), len(dn)
+    try:
+        if m[i:i+len_do] == do:
+            return "do"
+        elif m[i:i+len_dn] == dn:
+            return "do_not"
+    except:
+        return None
+
+
+def day_3_1(s: str):
+    mul_container = []
+    for i in range(len(s)):
+        if i == len(s) - 1:
+            break
+        else:
+            if s[i] == "m":
+                mul_container.append(find_match(s, i))
+    return summation_mul(mul_container)
+
+
+def day_3_2(s: str):
+    mul_container = []
+    do_flag = "do"
+    for i in range(len(s)):
+        if s[i] == "m":
+            if do_flag=="do":
+                mul_container.append(find_match(s, i))
+        elif s[i] == "d":
+            if mul_flag(s, i) is not None:
+                do_flag = mul_flag(s, i)
+    return summation_mul(mul_container)
+
+
+test_1 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+test_2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
+print(f'Solution 3.1 : {day_3_1(str_03)}')
+print(f'Solution 3.2 : {day_3_2(str_03)}')
--- 2024/utils.py
+++ 2024/utils.py
@@ -1,20 +1,13 @@
 import os
-from typing import Any
 
 import pandas as pd
 import numpy as np
 
 
-def load_data(file_name: str):
-    """
-    Load data for each riddle
-    :param file_name: name of the file to load e.g. "day_1.txt"
-    :type file_name: str
-    :return: data as np.array or pd.DataFrame values
-    """
+def load_array(file_name: str):
     file_path = os.path.join(file_name)
-    try:
-        return np.loadtxt(file_path)
-    except:
-        data = pd.read_csv(file_path, header=None)
-        return data.values
+    return np.loadtxt(file_path)
+
+def load_csv(file_name: str):
+    data = pd.read_csv(file_name, header=None)
+    return data.values
